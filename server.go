3499cb50c87b9fbac8a03b48faf6e02dec36178a
YuheiNakasaka <yuhei.nakasaka@gmail.com>
2014-09-10 20:25:20 +0900

    first commit

---
+++ server.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"github.com/go-martini/martini"
+)
+
+func main() {
+	m := martini.Classic()
+	m.Get("/", top)
+	m.Run()
+}
+
+func top(params martini.Params) (int, string) {
+	return 200, "Hello World!"
+}
